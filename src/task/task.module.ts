cc8a41facb5c8130047c5ffb2786ad1a50e1c73a
OEH808 <omarehababdelaziz8@gmail.com>
2023-09-30 13:32:41 +0300

    Created task schema

--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TaskController } from './task.controller';
 import { TaskService } from './task.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Task, taskSchema } from './task.schema';
 
 @Module({
+  imports: [MongooseModule.forFeature([{name: Task.name, schema: taskSchema}])],
   controllers: [TaskController],
   providers: [TaskService]
 })
---
+++ src/task/task.schema.ts
@@ -0,0 +1,27 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { IsEnum } from 'class-validator';
+import { HydratedDocument } from 'mongoose';
+import { TaskStatus } from '../constants/enums';
+
+@Schema()
+export class Task {
+    @Prop({ required: [true, ""], unique: true, trim: true })
+    id : number;
+
+    @Prop({ required: [true, ""], unique: true, trim: true })
+    title : string;
+
+    @Prop({ type: String, enum: TaskStatus, default: TaskStatus.OPEN })
+    //@IsEnum(TaskStatus)
+    status : TaskStatus;
+
+    @Prop({ trim: true })
+    time : string;
+
+    @Prop({ required: [true, "Task must have associated User"], trim: true })
+    user : string;
+}
+
+export type taskDocument = HydratedDocument<Task>;
+
+export const taskSchema = SchemaFactory.createForClass(Task);
