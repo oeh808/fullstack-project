746aaf58768c9d1d8ec6985009454de9a6f17bbb
OEH808 <omarehababdelaziz8@gmail.com>
2023-09-30 13:10:13 +0300

    Set up mongoose and validation pipe

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,40 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { TaskModule } from './task/task.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService} from '@nestjs/config';
+import { APP_PIPE } from '@nestjs/core';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`
+    }),
+    UserModule, 
+    TaskModule,  
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],  
+      useFactory: (config: ConfigService) => {
+        return {
+          uri: config.get<string>('DB_URI'),
+          dbName: config.get<string>('DB_NAME')
+        }
+      }
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      // Globaly scoped validation pipe
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        whitelist: true,
+      })
+    }
+  ],
 })
 export class AppModule {}
