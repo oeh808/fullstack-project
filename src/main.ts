e6b9c95132835f8471857e777bad81ee1832b210
OEH808 <omarehababdelaziz8@gmail.com>
2023-09-30 15:20:35 +0300

    Added console log for getting address

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+  // Makes a console log of the url being used by the nest app
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
