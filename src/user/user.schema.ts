efbc2128b3d637f30845ecbded3007f4a0d44fa9
OEH808 <omarehababdelaziz8@gmail.com>
2023-09-30 15:21:00 +0300

    Refactored hashing to the user service

--- src/user/user.schema.ts
+++ src/user/user.schema.ts
@@ -1,8 +1,5 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { HydratedDocument } from 'mongoose';
-import { randomBytes, scrypt as _scrypt } from 'crypto';
-import { promisify } from 'util';
-const scrypt = promisify(_scrypt);
 
 @Schema()
 export class User {
@@ -20,14 +17,3 @@ export class User {
 export type userDocument = HydratedDocument<User>;
 
 export const userSchema = SchemaFactory.createForClass(User);
-
-// Middleware to be called whenever a user signs up to hash their password
-userSchema.pre('save', async function() {
-    if (!this.isModified('password')){
-        return;
-    }
-    const salt = randomBytes(8).toString('hex');
-    const hash = (await scrypt(this.password, salt, 32)) as Buffer;
-    this.password = salt + '.' + hash.toString('hex');
-    console.log(this.password); 
-})
