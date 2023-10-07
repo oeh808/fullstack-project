952b667b9614fcd0590ea609f1acb263b9e02750
OEH808 <omarehababdelaziz8@gmail.com>
2023-10-07 13:20:56 +0300

    Created UI for login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,42 @@
 import React, { Component }  from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Alert from 'react-bootstrap/Alert'
+import Breadcrumb from 'react-bootstrap/Breadcrumb'
+import Container from 'react-bootstrap/Container'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <p className='col-xs-12 h1 mb-0 font-weight-bold'>Log in</p>
+      &nbsp;
+
+      <Container fluid className='w-25'>
+      <Form>
+        <Form.Group className="mb-3" controlId="formEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="name@example.com" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="" />
+        </Form.Group>
+        <Button type='submit'>Submit</Button>
+      </Form>
+      </Container>
+        
+      {/* <Breadcrumb variant='success'>
+        <Breadcrumb.Item>Test 1</Breadcrumb.Item>
+        <Breadcrumb.Item>Test 2</Breadcrumb.Item>
+        <Breadcrumb.Item active>Test 3</Breadcrumb.Item>
+      </Breadcrumb>
+
+      <Alert variant='danger'>This is a button</Alert>
+
+      <Button>Test Button</Button> */}
       </header>
     </div>
   );
