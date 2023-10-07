556c035d74d882415d4d8623d156712d4fe9152e
OEH808 <omarehababdelaziz8@gmail.com>
2023-10-07 22:38:58 +0300

    Updated Tasks page layout

--- frontend/src/pages/tasks.js
+++ frontend/src/pages/tasks.js
@@ -1,11 +1,28 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { Container, Form, Button, InputGroup } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
 
 const Tasks = () => {
     return (
         <div className="App">
             <header className="App-header">
-                <h1 className="text-center">Tasks Page</h1>
+            <Container fluid className='w-25'>
+                <h1 className="text-center fixed-top">Tasks Page</h1>
+                <InputGroup className="mb-3">
+                    
+                    <Form.Control
+                    placeholder="Enter Title..."
+                    aria-label="Title Search"
+                    aria-describedby="basic-addon1"
+                    />
+                    <Button>Search</Button>
+                </InputGroup>
+            </Container>
+            <Link to={'/'}>
+                <Button className="fixed-bottom">Sign Out</Button>
+            </Link>
             </header>
         </div>
     )
