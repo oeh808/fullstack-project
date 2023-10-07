d40b4088a9c58e447d62eb81209c35dfcba92b17
OEH808 <omarehababdelaziz8@gmail.com>
2023-10-07 22:18:05 +0300

    Added css to task page

--- frontend/src/pages/tasks.js
+++ frontend/src/pages/tasks.js
@@ -1,7 +1,14 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 const Tasks = () => {
-    return <h1>Tasks Page</h1>;
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1 className="text-center">Tasks Page</h1>
+            </header>
+        </div>
+    )
 }
 
 export default Tasks;
