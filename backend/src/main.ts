a53262a893e93350ddcb089340fe9cec560c5d88
OEH808 <omarehababdelaziz8@gmail.com>
2023-10-09 14:48:28 +0300

    Enabld Cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   await app.listen(3000);
   // Makes a console log of the url being used by the nest app
   console.log(`Application is running on: ${await app.getUrl()}`);
