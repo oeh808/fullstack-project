2a745ff1f4fa75d1e2ec5006092b93aa5f8d11b0
OEH808 <omarehababdelaziz8@gmail.com>
2023-10-09 14:48:38 +0300

    Added error handling

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Session, Get, Patch, Delete, Param, Query, Headers, UseGuards } from '@nestjs/common';
+import { Body, Controller, Post, Session, Get, Patch, Delete, Param, Query, Headers, UseGuards, BadRequestException } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { User } from './user.schema';
@@ -10,21 +10,32 @@ export class UserController {
 
     @Post('/signup')
     async signUp(@Body() body: CreateUserDto) {
-        const user = await this.userService.signUp(body.userID, body.email, body.password);
-
+        try{
+            const user = await this.userService.signUp(body.userID, body.email, body.password);
         return user;
+        }catch(error){
+            throw new BadRequestException(error.message);
+        }
     }
 
     @Post('/signin')
     async signIn(@Body() body: Partial<User>) {
-        const user = await this.userService.signIn(body.email, body.password);
-
-        return user;
+        try{
+            const user = await this.userService.signIn(body.email, body.password);
+            return user;
+        }catch(error){
+            throw new BadRequestException(error.message);
+        }
     }
 
     @UseGuards(JwtAuthGuard)
     @Get('WhoAmI')
-    whoAmI (@Headers('authorization') header: string) {
-        return this.userService.whoAmI(header.split(' ')[1]);
+    async whoAmI (@Headers('authorization') header: string) {
+        try{
+            return await this.userService.whoAmI(header.split(' ')[1]);
+        }catch(error){
+            throw new BadRequestException(error.message);
+        }
+        
     }
 }
